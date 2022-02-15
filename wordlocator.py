32b07ee8e8506b0180b5df04645d8dac048b7182
Joseph-Zaki <josephzaki123@gmail.com>
2022-02-14 19:10:50 -0800

    Finished wordlocator.py and transferred word bank to words.csv

--- wordlocator.py
+++ wordlocator.py
@@ -1,9 +1,16 @@
 import csv
-
+"""
+DANGER:
+This file can crash your system. Believe me.
+Writes from test.csv to words.csv to copy the list of possible wordle words.
+"""
 def main():
-    with open("test.csv") as f, open("words.csv", "a") as wordslist:
-        reader = csv.reader(f)
+    with open("test.csv", "r") as f, open("words.csv", "a", newline="") as wordslist:
+        reader = csv.reader(f) #returns 2D Array
         writer = csv.writer(wordslist)
-        wordslist.write("TestTest1")
-
+        wordsOnly = []
+        for row in reader:
+            wordsOnly.append([row[2]]) # appends list containing a word as one string
+        for word in wordsOnly:
+            writer.writerow(word) # writes each "row" which is one word to the file
 main()
